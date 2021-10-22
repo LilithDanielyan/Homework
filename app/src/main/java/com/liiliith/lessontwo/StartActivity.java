9d1ad5b37f42ea4e96186d6de4f1c8bf916d30f5
Gor646 <64868402+Gor646@users.noreply.github.com>
2021-10-22 13:08:21 +0200

    HomeWork3

---
+++ app/src/main/java/com/liiliith/lessontwo/StartActivity.java
@@ -0,0 +1,15 @@
+package com.liiliith.lessontwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
